e35bfaf1c3bcc16c1c261538654f7c995c81fc88
meta-ela <anxhelameta@live.it>
2022-05-27 17:31:20 +0200

    Adds cell creation and styling

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ Generare una griglia di gioco quadrata ,
 in cui ogni cella contiene un numero incrementale tra quelli compresi tra 1 e 100
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
-- creare griglia di celle grandi 40px x 40px 
+- creare griglia di celle quadrate (x =  y)
 - ciclo for per 100 celle
     - una cella è un div --> tramite innerhtml??
     - 
@@ -13,16 +13,20 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 // variabile globale per recuperare classe html che conterrà la griglia
 const gridContainer = document.querySelector(".grid_container");
 
-// funzione per generare la griglia
-function createGrid(xCells, yCells) {
-    const cellsGrid = xCells * yCells;
+// funzione per generare la griglia da 1 a 100
+function createGrid() {
+    const cellsGrid = 100;
 
     console.log(cellsGrid);
 
     for (let i = 1; i <= cellsGrid; i++) {
         const cell = document.createElement("div");
+        // aggiunge alla variabile cell la classe cell con gli stili necessari
+        cell.classList.add("cell");
         gridContainer.append(cell);
     }
+
+    
 }
 
-createGrid(1,100);
+createGrid();
